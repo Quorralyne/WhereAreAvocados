858f3d7f1edfd257ea70eb9bb7d1075cae0cf6cf
Heather Downing <quorralyne@gmail.com>
2019-03-29 09:27:24 +0100

    Added post, put and delete actions to locaiton controller. Updated all dependancy packages.

--- WhereAreAvocados/Controllers/LocationController.cs
+++ WhereAreAvocados/Controllers/LocationController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using WhereAreAvocados.Data.Models.Data;
 
@@ -18,23 +21,66 @@ namespace WhereAreAvocados.Controllers
         [HttpGet, Route("api/location/events")]
         public List<Event> GetAllEvents()
         {
-            return SampleEvents();
+            return _sampleEvents;
         }
 
         // GET: api/Location/FutureEvents
         [HttpGet, Route("api/location/futureevents")]
         public List<Event> GetFutureEvents()
         {
-            return SampleFutureEvents();
+            return _sampleFutureEvents();
+        }
+
+        // POST: api/Location/
+        public HttpResponseMessage Post([FromBody]Event model)
+        {
+            if (string.IsNullOrEmpty(model?.EventName))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var maxId = 0;
+            if (_sampleEvents.Count > 0)
+            {
+                maxId = _sampleEvents.Max(x => x.Id);
+            }
+            model.Id = maxId + 1;
+            _sampleEvents.Add(model);
+            return Request.CreateResponse(HttpStatusCode.Created, model);
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]Event model)
+        {
+            if (string.IsNullOrEmpty(model?.EventName))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var item = _sampleEvents.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                // Update *all* of the item's properties
+                item = model;
+
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            var item = _sampleEvents.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                _sampleEvents.Remove(item);
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
         }
 
         #region Sample Data Methods
 
-        private static List<Event> SampleEvents()
+        private static List<Event> _sampleEvents { get; set; } = new List<Event>()
         {
-            return new List<Event>()
-            {
-                new Event()
+            new Event()
                 {
                     Id = 1,
                     EventName = "Event 1",
@@ -54,55 +100,55 @@ namespace WhereAreAvocados.Controllers
                         }
                     }
                 },
-                new Event()
+            new Event()
+            {
+                Id = 2,
+                EventName = "Event 2",
+                Website = "https://event2.com",
+                Location = "Anytown, Australia",
+                Latitude = "lat 2",
+                Longitude = "long 2",
+                StartDate = new DateTime(2019,2,2),
+                EndDate = new DateTime(2019,2,4),
+                Advocates = new List<Advocate>()
                 {
-                    Id = 2,
-                    EventName = "Event 2",
-                    Website = "https://event2.com",
-                    Location = "Anytown, Australia",
-                    Latitude = "lat 2",
-                    Longitude = "long 2",
-                    StartDate = new DateTime(2019,2,2),
-                    EndDate = new DateTime(2019,2,4),
-                    Advocates = new List<Advocate>()
+                    new Advocate()
                     {
-                        new Advocate()
-                        {
-                            Id = 200,
-                            Name = "John Smith",
-                            TwitterHandle = "johnsmith"
-                        }
-                    }
-                },
-                new Event()
-                {
-                    Id = 3,
-                    EventName = "Event 3",
-                    Website = "https://event3.com",
-                    Location = "Anytown, Canada",
-                    Latitude = "lat 3",
-                    Longitude = "long 3",
-                    StartDate = new DateTime(2019,3,3),
-                    EndDate = new DateTime(2019,3,7),
-                    Advocates = new List<Advocate>()
-                    {
-                        new Advocate()
-                        {
-                            Id = 300,
-                            Name = "Alex Brown",
-                            TwitterHandle = "alexbrown"
-                        },
-                        new Advocate()
-                        {
-                            Id = 100,
-                            Name = "Jane Doe",
-                            TwitterHandle = "janedoe"
-                        }
+                        Id = 200,
+                        Name = "John Smith",
+                        TwitterHandle = "johnsmith"
                     }
                 }
-            };
-        }
-        private static List<Event> SampleFutureEvents()
+            },
+            new Event()
+            {
+                Id = 3,
+                EventName = "Event 3",
+                Website = "https://event3.com",
+                Location = "Anytown, Canada",
+                Latitude = "lat 3",
+                Longitude = "long 3",
+                StartDate = new DateTime(2019,3,3),
+                EndDate = new DateTime(2019,3,7),
+                Advocates = new List<Advocate>()
+                {
+                    new Advocate()
+                    {
+                        Id = 300,
+                        Name = "Alex Brown",
+                        TwitterHandle = "alexbrown"
+                    },
+                    new Advocate()
+                    {
+                        Id = 100,
+                        Name = "Jane Doe",
+                        TwitterHandle = "janedoe"
+                    }
+                }
+            }
+        };
+
+        private static List<Event> _sampleFutureEvents()
         {
             return new List<Event>()
             {
